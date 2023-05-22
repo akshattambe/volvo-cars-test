e1126f91f23e3a8a3feb7c897ee213e55b070f5f
akshat.tambe <akshat.tambe@docusign.com>
2023-05-22 17:35:56 +0200

    AppiumServiceTool.java

--- AppiumService/src/main/java/org/example/AppiumServiceTool.java
+++ AppiumService/src/main/java/org/example/AppiumServiceTool.java
@@ -4,10 +4,44 @@ package org.example;
  * Hello world!
  *
  */
-public class App 
+@Singleton
+public final class AppiumServiceTool
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    private static AppiumServiceTool appiumServiceTool;
+
+    AppiumDriverLocalService appiumDriverLocalService;
+
+    private AppiumDriverService(){
+
+    }
+
+    public static AppiumServiceTool getAppiumDriverServiceToolInstance(){
+        if(AppiumServiceTool == null) {
+            AppiumServiceTool = new AppiumServiceTool();
+        }
+        return appiumServiceTool;
+    }
+
+    public AppiumDriverLocalService startAppiumService(){
+        if(appiumDriverLocalService!= null) {
+            appiumDriverLocalService.stop();
+        }
+
+        appiumDriverLocalService = AppiumDriverLocalService.buildDefaultService();
+        appiumDriverLocalService.start();
+
+        System.out.println("Appium Server started: " + appiumDriverLocalService.getUrl().toString());
+        return appiumDriverLocalService;
+    }
+
+    /**
+     * Stop Appium Service after quiting the driver.
+     * @param appiumDriverLocalService
+     */
+    public void stopAppiumService(AppiumDriverLocalService appiumDriverLocalService){
+        if (appiumDriverLocalService.isRunning()) {
+            appiumDriverLocalService.stop();
+            System.out.println("Appium Server stopped: " + appiumDriverLocalService.getUrl().toString());
+        }
     }
 }
