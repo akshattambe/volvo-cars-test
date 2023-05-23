7fd7a856bc68d03a4e043b70ef061834faa2fad1
akshat.tambe <akshat.tambe@docusign.com>
2023-05-23 12:34:42 +0200

    test are actully working now.

--- ios_test/src/test/java/com/example/AppiumDriverServiceTest.java
+++ ios_test/src/test/java/com/example/AppiumDriverServiceTest.java
@@ -28,28 +28,75 @@ public class AppiumDriverServiceTest {
     @Test
     public void test() throws InterruptedException {
         System.out.println("test is good.");
-        boolean isDisplayedSwitches = iosDriver.findElement(AppiumBy.accessibilityId("Switches")).isDisplayed();
-        Thread.sleep(3000);
 
+        /**
+         * 1. Click on Switches
+         */
+        boolean isDisplayedSwitches = iosDriver.findElement(AppiumBy.accessibilityId("Switches")).isDisplayed();
         Assert.assertTrue(isDisplayedSwitches);
         iosDriver.findElement(AppiumBy.accessibilityId("Switches")).click();
-        isDisplayedSwitches = false;
-        Thread.sleep(3000);
 
-//        iosDriver.findElement(AppiumBy.accessibilityId("**/XCUIElementTypeSwitch[`value == \"1\"`][2]")).click();
-//        Thread.sleep(3000);
-//
-//        boolean isDisplayedTintedOff = iosDriver.findElement(AppiumBy.accessibilityId("**/XCUIElementTypeSwitch[`value == \"0\"`]")).isDisplayed();
-//        Thread.sleep(3000);
-//
-//        Assert.assertTrue(isDisplayedTintedOff);
-        boolean asd = iosDriver.findElement(AppiumBy.accessibilityId("XCUIElementTypeButton")).isDisplayed();
-        iosDriver.findElement(AppiumBy.accessibilityId("XCUIElementTypeButton")).click(); //click UiKitCatalog back button.
-        Thread.sleep(3000);
+        /**
+         * change TINTED to OFF.
+         */
+        boolean asd = iosDriver.findElement(AppiumBy.iOSClassChain("**/XCUIElementTypeSwitch[`value == \"1\"`][2]")).isDisplayed();
+        Assert.assertTrue(asd);
+        iosDriver.findElement(AppiumBy.iOSClassChain("**/XCUIElementTypeSwitch[`value == \"1\"`][2]")).click();
+        //XCUIElementTypeApplication[@name="UIKitCatalog"]/XCUIElementTypeWindow/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeTable/XCUIElementTypeCell[2]/XCUIElementTypeSwitch
 
-        isDisplayedSwitches = iosDriver.findElement(AppiumBy.accessibilityId("Switches")).isDisplayed();
-        Assert.assertTrue(isDisplayedSwitches);
-        Thread.sleep(3000);
+        /**
+         * Click on UiKitCatalog back button.
+         */
+        boolean backButton = iosDriver.findElement(AppiumBy.className("XCUIElementTypeButton")).isDisplayed();
+        Assert.assertTrue(backButton);
+        iosDriver.findElement(AppiumBy.className("XCUIElementTypeButton")).click(); //click UiKitCatalog back button.
 
+
+        /**
+         * 2. Click on Steppers.
+         */
+        iosDriver.findElement(AppiumBy.accessibilityId("Steppers")).click();
+
+        /**
+         * Click on + in the TINTED until value will be 10.
+         */
+
+        for (int i = 1; i <= 10; i++) {
+            // Perform actions for each iteration
+            iosDriver.findElement(AppiumBy.iOSClassChain("**/XCUIElementTypeButton[`label == \"Increment\"`][2]")).click();
+            //Check the value of TINTED.
+            Assert.assertTrue(iosDriver.findElement(AppiumBy.accessibilityId(Integer.toString(i))).getText().equalsIgnoreCase(Integer.toString(i)));
+        }
+
+        /**
+         * Click on UiKitCatalog back button.
+         */
+        backButton = iosDriver.findElement(AppiumBy.className("XCUIElementTypeButton")).isDisplayed();
+        Assert.assertTrue(backButton);
+        iosDriver.findElement(AppiumBy.className("XCUIElementTypeButton")).click(); //click UiKitCatalog back button.
+        Thread.sleep(1000);
+
+        /**
+         * 3. Click on Sliders.
+         */
+        iosDriver.findElement(AppiumBy.accessibilityId("Sliders")).click();
+        /**
+         * Click on UiKitCatalog back button.
+         */
+        backButton = iosDriver.findElement(AppiumBy.className("XCUIElementTypeButton")).isDisplayed();
+        Assert.assertTrue(backButton);
+        iosDriver.findElement(AppiumBy.className("XCUIElementTypeButton")).click(); //click UiKitCatalog back button.
+        Thread.sleep(1000);
+
+        /**
+         * 4. Click on Picker View.
+         */
+        iosDriver.findElement(AppiumBy.accessibilityId("Picker View")).click();
+        /**
+         * Click on UiKitCatalog back button.
+         */
+        backButton = iosDriver.findElement(AppiumBy.className("XCUIElementTypeButton")).isDisplayed();
+        Assert.assertTrue(backButton);
+        iosDriver.findElement(AppiumBy.className("XCUIElementTypeButton")).click(); //click UiKitCatalog back button.
     }
 }
