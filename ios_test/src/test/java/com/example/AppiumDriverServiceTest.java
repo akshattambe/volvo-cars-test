4727893698952e041fd714f80a26ce383be3d3c8
akshat.tambe <akshat.tambe@docusign.com>
2023-05-24 08:19:44 +0200

    Slider is working

--- ios_test/src/test/java/com/example/AppiumDriverServiceTest.java
+++ ios_test/src/test/java/com/example/AppiumDriverServiceTest.java
@@ -2,9 +2,27 @@ package com.example;
 
 import com.example.service.AppiumDriverService;
 import io.appium.java_client.AppiumBy;
+import io.appium.java_client.TouchAction;
 import io.appium.java_client.ios.IOSDriver;
 import io.appium.java_client.service.local.AppiumDriverLocalService;
+import io.appium.java_client.touch.WaitOptions;
+import io.appium.java_client.touch.offset.ElementOption;
 import org.junit.*;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Pause;
+import org.openqa.selenium.interactions.PointerInput;
+import org.openqa.selenium.interactions.Sequence;
+
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.Collections;
+
+import static io.appium.java_client.touch.WaitOptions.waitOptions;
+import static io.appium.java_client.touch.offset.PointOption.point;
+import static java.time.Duration.ofMillis;
+
+
 
 public class AppiumDriverServiceTest {
     private AppiumDriverService appiumDriverService;
@@ -80,6 +98,14 @@ public class AppiumDriverServiceTest {
          * 3. Click on Sliders.
          */
         iosDriver.findElement(AppiumBy.accessibilityId("Sliders")).click();
+
+        /**
+         * Change the TINTED slider to the Maximum.
+         */
+        String xpath = "//XCUIElementTypeApplication[@name=\"UIKitCatalog\"]/XCUIElementTypeWindow/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeTable/XCUIElementTypeCell[2]/XCUIElementTypeSlider";
+        WebElement slider = iosDriver.findElement(AppiumBy.xpath(xpath));
+        slider.sendKeys("1");
+
         /**
          * Click on UiKitCatalog back button.
          */
@@ -92,11 +118,40 @@ public class AppiumDriverServiceTest {
          * 4. Click on Picker View.
          */
         iosDriver.findElement(AppiumBy.accessibilityId("Picker View")).click();
+
+        /**
+         * Set Picker Views -> Select 80 200 100.
+         */
+
+
         /**
          * Click on UiKitCatalog back button.
          */
         backButton = iosDriver.findElement(AppiumBy.className("XCUIElementTypeButton")).isDisplayed();
         Assert.assertTrue(backButton);
         iosDriver.findElement(AppiumBy.className("XCUIElementTypeButton")).click(); //click UiKitCatalog back button.
+
+        /**
+         * Put application in the Background ,Open Last Message in the Mobile / Simulator and then open application again.
+         */
+    }
+
+    //Horizontal Swipe by percentages
+    public void slideSliderToEnd(WebElement sliderElement, IOSDriver driver) {
+//        int startX = sliderElement.getLocation().getX();
+//        int endX = startX + sliderElement.getSize().getWidth();
+//        int centerY = sliderElement.getLocation().getY() + sliderElement.getSize().getHeight() / 2;
+//
+//        TouchAction<?> touchAction = new TouchAction<>(driver);
+//        touchAction.press(ElementOption.element(sliderElement, startX, centerY))
+//                .moveTo(ElementOption.element(sliderElement, endX, centerY))
+//                .release()
+//                .perform();
+
+        String sliderId = sliderElement.getAttribute("elementId");
+        String script = "mobile: scroll";
+        String direction = "right";
+
+        iosDriver.executeScript(script, "{\"element\": \"" + sliderId + "\", \"direction\": \"" + direction + "\"}");
     }
 }
