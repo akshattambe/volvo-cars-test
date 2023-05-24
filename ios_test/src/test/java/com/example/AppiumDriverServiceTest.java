5b24b562c83a91f66a174b47f6e6c650124a4297
akshat.tambe <akshat.tambe@docusign.com>
2023-05-24 08:52:30 +0200

    All test are working

--- ios_test/src/test/java/com/example/AppiumDriverServiceTest.java
+++ ios_test/src/test/java/com/example/AppiumDriverServiceTest.java
@@ -2,25 +2,10 @@ package com.example;
 
 import com.example.service.AppiumDriverService;
 import io.appium.java_client.AppiumBy;
-import io.appium.java_client.TouchAction;
 import io.appium.java_client.ios.IOSDriver;
 import io.appium.java_client.service.local.AppiumDriverLocalService;
-import io.appium.java_client.touch.WaitOptions;
-import io.appium.java_client.touch.offset.ElementOption;
 import org.junit.*;
-import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Pause;
-import org.openqa.selenium.interactions.PointerInput;
-import org.openqa.selenium.interactions.Sequence;
-
-import java.time.Duration;
-import java.util.Arrays;
-import java.util.Collections;
-
-import static io.appium.java_client.touch.WaitOptions.waitOptions;
-import static io.appium.java_client.touch.offset.PointOption.point;
-import static java.time.Duration.ofMillis;
 
 
 
@@ -57,10 +42,14 @@ public class AppiumDriverServiceTest {
         /**
          * change TINTED to OFF.
          */
-        boolean asd = iosDriver.findElement(AppiumBy.iOSClassChain("**/XCUIElementTypeSwitch[`value == \"1\"`][2]")).isDisplayed();
-        Assert.assertTrue(asd);
-        iosDriver.findElement(AppiumBy.iOSClassChain("**/XCUIElementTypeSwitch[`value == \"1\"`][2]")).click();
-        //XCUIElementTypeApplication[@name="UIKitCatalog"]/XCUIElementTypeWindow/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeTable/XCUIElementTypeCell[2]/XCUIElementTypeSwitch
+        WebElement tintOn = iosDriver.findElement(AppiumBy.iOSClassChain("**/XCUIElementTypeSwitch[`value == \"1\"`][2]"));
+        Assert.assertTrue(tintOn.isDisplayed());
+
+        String initialValue = tintOn.getAttribute("value");
+        tintOn.click();
+        WebElement tintOff = iosDriver.findElement(AppiumBy.iOSClassChain("**/XCUIElementTypeSwitch[`value == \"0\"`]"));
+        String finalValue = tintOff.getAttribute("value");
+        Assert.assertNotEquals(initialValue, finalValue);
 
         /**
          * Click on UiKitCatalog back button.
@@ -122,6 +111,18 @@ public class AppiumDriverServiceTest {
         /**
          * Set Picker Views -> Select 80 200 100.
          */
+        WebElement red = iosDriver.findElement(AppiumBy.accessibilityId("Red color component value"));
+        Assert.assertTrue(red.isDisplayed());
+        red.sendKeys("80");
+        Assert.assertTrue(red.getText().equalsIgnoreCase("80"));
+        WebElement green = iosDriver.findElement(AppiumBy.accessibilityId("Green color component value"));
+        Assert.assertTrue(green.isDisplayed());
+        green.sendKeys("200");
+        Assert.assertTrue(green.getText().equalsIgnoreCase("200"));
+        WebElement blue = iosDriver.findElement(AppiumBy.accessibilityId("Blue color component value"));
+        Assert.assertTrue(blue.isDisplayed());
+        blue.sendKeys("100");
+        Assert.assertTrue(blue.getText().equalsIgnoreCase("100"));
 
 
         /**
@@ -134,24 +135,5 @@ public class AppiumDriverServiceTest {
         /**
          * Put application in the Background ,Open Last Message in the Mobile / Simulator and then open application again.
          */
-    }
-
-    //Horizontal Swipe by percentages
-    public void slideSliderToEnd(WebElement sliderElement, IOSDriver driver) {
-//        int startX = sliderElement.getLocation().getX();
-//        int endX = startX + sliderElement.getSize().getWidth();
-//        int centerY = sliderElement.getLocation().getY() + sliderElement.getSize().getHeight() / 2;
-//
-//        TouchAction<?> touchAction = new TouchAction<>(driver);
-//        touchAction.press(ElementOption.element(sliderElement, startX, centerY))
-//                .moveTo(ElementOption.element(sliderElement, endX, centerY))
-//                .release()
-//                .perform();
-
-        String sliderId = sliderElement.getAttribute("elementId");
-        String script = "mobile: scroll";
-        String direction = "right";
-
-        iosDriver.executeScript(script, "{\"element\": \"" + sliderId + "\", \"direction\": \"" + direction + "\"}");
     }
 }
